bbba03ca63df327c0e24a26b71ee319767ef1292
sittisak <sittisak.churitayo@gmail.com>
2018-11-08 17:22:23 +0700

    getIdBytes return ByteBuffer type

--- src/ChaincodeProposalCreator.ts
+++ src/ChaincodeProposalCreator.ts
@@ -1,24 +1,25 @@
-import { SerializedIdentity } from 'fabric-shim';
-
+// import { SerializedIdentity } from 'fabric-shim';
+import ByteBuffer from 'bytebuffer';
 /**
  * @hidden
  */
-export class ChaincodeProposalCreator implements SerializedIdentity {
+// export class ChaincodeProposalCreator implements SerializedIdentity {
+export class ChaincodeProposalCreator {
     // tslint:disable-next-line:variable-name
-    id_bytes: Buffer;
+    id_bytes: ByteBuffer;
 
     [fieldName: string]: any;
     mspid: string;
 
     constructor(private mspId: string, private signingId: string) {
-        this.id_bytes = Buffer.from(signingId);
+        this.id_bytes = new ByteBuffer().writeIString(signingId).flip();
     }
 
     getMspid(): string {
         return this.mspId;
     }
 
-    getIdBytes(): Buffer {
+    getIdBytes(): ByteBuffer {
         return this.id_bytes;
     }
 }
